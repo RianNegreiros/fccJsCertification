7467e727cc0a12e6e77f6b01631aaa6e169ca17d
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2022-01-23 00:22:45 -0300

    rot13

--- caesarsCipher/rot13.js
+++ caesarsCipher/rot13.js
@@ -1,5 +1,16 @@
 function rot13(str) {
-  return str;
+  let cipher = str.split(' ')
+  for (let i = 0; i < str.length; i++) {
+    let charCode = str.charCodeAt(i)
+    let fromCharCode = String.fromCharCode(charCode)
+    console.log(fromCharCode)
+    if ((/[a-zA-Z]/).test(fromCharCode) === true) {
+      cipher[i] = String.fromCharCode(((charCode - 65 + 13) % 26) + 65)
+    } else {
+      cipher[i] = fromCharCode
+    }
+  }
+  return cipher.join('')
 }
 
-rot13("SERR PBQR PNZC");
+console.log(rot13('SERR PBQR PNZC'))
