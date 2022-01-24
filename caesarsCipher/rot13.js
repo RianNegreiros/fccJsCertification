37bfb0aee2b6173f047079ad638ffad91acbd545
Rian Negreiros <riannegreiros@gmail.com>
2022-01-24 01:49:15 +0000

    refactor

--- caesarsCipher/rot13.js
+++ caesarsCipher/rot13.js
@@ -3,8 +3,7 @@ function rot13(str) {
   for (let i = 0; i < str.length; i++) {
     let charCode = str.charCodeAt(i)
     let fromCharCode = String.fromCharCode(charCode)
-    console.log(fromCharCode)
-    if ((/[a-zA-Z]/).test(fromCharCode) === true) {
+    if ((/[a-zA-Z]/).test(fromCharCode)) {
       cipher[i] = String.fromCharCode(((charCode - 65 + 13) % 26) + 65)
     } else {
       cipher[i] = fromCharCode
